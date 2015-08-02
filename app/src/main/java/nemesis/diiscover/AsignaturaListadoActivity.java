59972a642afbeacd8ef0353ada959931b23acc91
Jorge Cáncer GIl <646122@unizar.es>
2015-08-02 21:40:01 +0200

    Arreglos de inconsistencias

--- app/src/main/java/nemesis/diiscover/AsignaturaListadoActivity.java
+++ app/src/main/java/nemesis/diiscover/AsignaturaListadoActivity.java
@@ -1,7 +1,7 @@
 package nemesis.diiscover;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
